ac4b4f0fadaae4444a0b4f3d2ba97307b121345e
Fenn Ignatius Saji <fennig1997@gmail.com>
2022-11-02 18:46:42 +0530

    Balance and did pallet script written

---
+++ src/balance.js
@@ -0,0 +1,36 @@
+const {utils} = require("metamui-sdk");
+const {bnToHex} = require('@polkadot/util');
+
+function generateBalances(balanceStore) {
+    let account_key = '0xc2261276cc9d1f8598ea4b6a74b15c2fb99d880ec681799c0cf30e8886371da9';
+    let locks_key = '0xc2261276cc9d1f8598ea4b6a74b15c2f218f26c73add634897550b4003b26bc6';
+    let total_issuance_key = '0xc2261276cc9d1f8598ea4b6a74b15c2f57c875e4cff74148e4628f264b974c80';
+
+    let new_account_key = '0xc2261276cc9d1f8598ea4b6a74b15c2fb99d880ec681799c0cf30e8886371da9';
+    let new_locks_key = '0xc2261276cc9d1f8598ea4b6a74b15c2f218f26c73add634897550b4003b26bc6';
+    let new_total_issuance_key = '0xc2261276cc9d1f8598ea4b6a74b15c2f57c875e4cff74148e4628f264b974c80';
+
+    let encodedValues = {};
+
+    for (const {key, value} of Object.values(balanceStore.accounts)) {
+        if(key.startsWith(account_key)) {
+            let updated_key = key.replace(account_key, new_account_key);
+            encodedValues[updated_key] = utils.encodeData(value, "AccountData");
+        }
+    }
+    for (const {key, value} of Object.values(balanceStore.locks)) {
+        if(key.startsWith(locks_key)) {
+            let updated_key = key.replace(locks_key, new_locks_key);
+            encodedValues[updated_key] = utils.encodeData(value, "Vec<BalanceLock>");
+        }
+    }
+    if(balanceStore.totalIssuance.key == total_issuance_key) {
+        encodedValues[new_total_issuance_key] = bnToHex(balanceStore.totalIssuance.value, { isLe: true }).toString();
+    }
+
+    return encodedValues;
+}
+
+module.exports = {
+    generateBalances,
+}
---
+++ src/did.js
@@ -0,0 +1,62 @@
+const {utils} = require("metamui-sdk");
+
+function generatedDids(didStore) {
+    let accounts_key = '0x8c00ca9d36dbd8b4d8e6b787982148bcb99d880ec681799c0cf30e8886371da9';
+    let dids_key = '0x8c00ca9d36dbd8b4d8e6b787982148bce395ab58338d83d25270a3a12ce872f8';
+    let lookup_key = '0x8c00ca9d36dbd8b4d8e6b787982148bc891ad457bf4da54990fa84a2acb148a2';
+    let rlookup_key = '0x8c00ca9d36dbd8b4d8e6b787982148bcf65cb14646e631fff440382018b1bfe4';
+    let prevkeys_key = '0x8c00ca9d36dbd8b4d8e6b787982148bc90cd84a4f521e9018818dffdac4a5860';
+
+    let new_accounts_key = '0xcb732bb8b688ea549fec1838a1350aedb99d880ec681799c0cf30e8886371da9';
+    let new_dids_key = '0x7a7b34fdb954e2e22000798738d613cde395ab58338d83d25270a3a12ce872f8';
+    let new_lookup_key = '0x7a7b34fdb954e2e22000798738d613cd891ad457bf4da54990fa84a2acb148a2';
+    let new_rlookup_key = '0x7a7b34fdb954e2e22000798738d613cdf65cb14646e631fff440382018b1bfe4';
+    let new_prevkeys_key = '0x7a7b34fdb954e2e22000798738d613cd90cd84a4f521e9018818dffdac4a5860';
+    
+    let encodedValues = {};
+
+    // Accounts is now moved to token.accounts
+    for (const {key, value} of Object.values(didStore.accounts)) {
+        let updated_key = key.replace(accounts_key, new_accounts_key);
+        let updated_value = {
+            nonce: value.nonce,
+            consumers: value.refcount,
+            providers: 1,
+            sufficients: 0,
+            data: value.data,
+        }
+        encodedValues[updated_key] = utils.encodeData(updated_value, "AccountInfoWithTripleRefCount");
+    }
+
+    for (const {key, value} of Object.values(didStore.dids)) {
+        let updated_key = key.replace(dids_key, new_dids_key);
+        let updated_value = [{
+            Private: {
+                ...value[0],
+                metadata: utils.encodeData(value[0].metadata.padEnd(32, '\0'), 'DidMetadata'),
+            }
+        }, 0];
+        encodedValues[updated_key] = utils.encodeData(updated_value, "(DIdentity, BlockNumber)");
+    }
+
+    for (const {key, value} of Object.values(didStore.lookups)) {
+        let updated_key = key.replace(lookup_key, new_lookup_key);
+        encodedValues[updated_key] = utils.encodeData(value, "AccountId");
+    }
+
+    for (const {key, value} of Object.values(didStore.rlookups)) {
+        let updated_key = key.replace(rlookup_key, new_rlookup_key);
+        encodedValues[updated_key] = utils.encodeData(value, "Did");
+    }
+
+    for (const {key, value} of Object.values(didStore.prevkeys)) {
+        let updated_key = key.replace(prevkeys_key, new_prevkeys_key);
+        encodedValues[updated_key] = utils.encodeData(value, "PrevKeysMap");
+    }
+
+    return encodedValues;
+}
+
+module.exports = {
+    generatedDids,
+}
